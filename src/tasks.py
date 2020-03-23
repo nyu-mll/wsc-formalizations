c56ca2be25dee3280230dec7be04037da668306c
Haokun Liu <haokunliu412@gmail.com>
2020-03-22 22:14:29 -0400

    add tasks skeleton

--- src/tasks.py
+++ src/tasks.py
@@ -3,6 +3,49 @@ from fairseq_wsc.wsc_utils import winogrande_jsonl_iterator
 
 
 class Task(object):
-    def __init__(self):
-        pass
+    def __init__(self, dataset, framing):
+        self.dataset = None
+        self.framing = None
+        self.raw_data = None
+        self.preprocessed_data = None
+        self.iterators = None
 
+    def preprocess_data(self):
+        raise NotImplementedError
+
+    def build_iterators(self):
+        raise NotImplementedError
+
+
+class WinograndeTask(Task):
+    def __init__(self, framing):
+        super().__init__(dataset="winogrande", framing=framing)
+        self.load_raw_data()
+        self.preprocess_data()
+        self.build_iterators()
+
+    def load_raw_data(self):
+        raise NotImplementedError
+
+
+class WSCTask(Task):
+    def __init__(self, framing):
+        super().__init__(dataset="wsc", framing=framing)
+        self.load_raw_data()
+        self.preprocess_data()
+        self.build_iterators()
+
+    def load_raw_data(self):
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    wsc_train = "/scratch/hl3236/data/WSC/train.jsonl"
+    wsc_val = "/scratch/hl3236/data/WSC/val.jsonl"
+    winogrande_train = "/scratch/hl3236/data/Winogrande/train_xl.jsonl"
+    winogrande_val = "/scratch/hl3236/data/Winogrande/dev.jsonl"
+    wsc_data = list(wsc_jsonl_iterator(wsc_val))
+    winogrande_data = list(winogrande_jsonl_iterator(winogrande_val))
+    import IPython
+
+    IPython.embed()
