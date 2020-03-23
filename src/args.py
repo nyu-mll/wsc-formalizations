93db139efb18d5ce241b9f5e825aeebfa554fe08
Haokun Liu <haokunliu412@gmail.com>
2020-03-23 03:03:55 -0400

    update args

--- src/args.py
+++ src/args.py
@@ -3,14 +3,13 @@ import os
 
 parser = argparse.ArgumentParser(description="WSC Trick Experiments")
 
-
 # basic settings
 # experiment name
 parser.add_argument("--exp-name", type=str, default="debug")
 # directory to save results, cached data, model states
-parser.add_argument("--results-dir", type=str, default="data/")
+parser.add_argument("--results-dir", type=str, default=os.getenv("NLU_DATA_DIR", "data/"))
 # directory in which data are stored
-parser.add_argument("--data-dir", type=str, default="results/")
+parser.add_argument("--data-dir", type=str, default=os.getenv("NLU_RESULT_DIR", "results/"))
 # mode
 parser.add_argument("--mode", type=str, default="train", choices=["train", "eval"])
 # load model state before training / evaluating
@@ -53,13 +52,15 @@ parser.add_argument("--lr", type=float, default=1e-5)
 # weight decay
 parser.add_argument("--weight-decay", type=float, default=1e-3)
 # number of epochs
-parser.add_argument("--num_epochs", type=int, default=10)
+parser.add_argument("--num-epochs", type=int, default=10)
 # propotion of warmup iters to full training process
 parser.add_argument("--warmup-iters-proportion", type=float, default=0.05)
 # number of iterations between validation
 parser.add_argument("--val-interval-iters", type=int, default=1000)
 # number of iterations between reporting result
 parser.add_argument("--report-interval-iters", type=int, default=200)
+# number of validations waiting for better results before stopping
+parser.add_argument("--stopping-patience", type=int, default=-1)
 
 
 # model settings
@@ -72,7 +73,7 @@ parser.add_argument(
 )
 
 
-def cfg_check(cfg):
+def check_config(cfg):
     if cfg.device == "cpu":
         cfg.dp = False
         cfg.amp = False
