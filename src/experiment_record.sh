902e37bd70d06fdf0b4cdc82052ee4434feb884a
Haokun Liu <haokunliu412@gmail.com>
2020-04-08 14:22:39 -0400

    reorder the resuls

--- src/experiment_record.sh
+++ src/experiment_record.sh
@@ -8,9 +8,9 @@ PROG="main" ARGS="--reload-data --exp-name mc-mlm --dataset wsc-cross --framing 
 
 
 {"best_acc": 0.7115384615384616, "best_iter": 1449, "current_iter": 3450, "exp_name": "p-span"}
-{"best_acc": 0.7403846153846154, "best_iter": 1056, "current_iter": 1600, "exp_name": "mc-sent-pair"}
 {"best_acc": 0.8076923076923077, "best_iter": 3036, "current_iter": 3450, "exp_name": "p-sent"}
-{"best_acc": 0.7211538461538461, "best_iter": 1512, "current_iter": 3150, "exp_name": "mc-sent-scale"}
 {"best_acc": 0.7211538461538461, "best_iter": 1380, "current_iter": 3450, "exp_name": "mc-sent-ploss"}
+{"best_acc": 0.7403846153846154, "best_iter": 1056, "current_iter": 1600, "exp_name": "mc-sent-pair"}
+{"best_acc": 0.7211538461538461, "best_iter": 1512, "current_iter": 3150, "exp_name": "mc-sent-scale"}
 {"best_acc": 0.7019230769230769, "best_iter": 1280, "current_iter": 1600, "exp_name": "mc-sent"}
 {"best_acc": 0.7019230769230769, "best_iter": 160, "current_iter": 1600, "exp_name": "mc-mlm"}
