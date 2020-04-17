fix apex, add gradient accumulation

src/trainer.py
@@ -8,7 +8,8 @@ class Trainer:
         self,
         model,
         task,
-        bs,
+        hardware_bs,
+        accumulation,
         lr,
         weight_decay,
         max_epochs,
@@ -21,13 +22,13 @@ class Trainer:
         self.task = task
         self.model = model
         self.task.preprocess_data(model=model)
-        self.task.build_iterators(bs=bs)
+        self.task.build_iterators(bs=hardware_bs)
         self.optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
         self.scheduler = torch.optim.lr_scheduler.OneCycleLR(
             self.optimizer,
             max_lr=lr,
             epochs=max_epochs,
-            steps_per_epoch=len(task.iterators["train"]),
+            steps_per_epoch=len(task.iterators["train"]) // accumulation,
             pct_start=warmup_iters_ratio,
             anneal_strategy="linear",
             cycle_momentum=False,
@@ -72,9 +73,9 @@ class Trainer:
         stopping = False
         for epoch in range(self.max_epochs):
             log.info(f"train epoch {epoch + 1} / {self.max_epochs}")
+            self.model.zero_grad()
 
             for batch, batch_inputs in enumerate(self.task.iterators["train"]):
-                self.model.zero_grad()
                 batch_outputs = self.model(self.move_inputs_to_device(batch_inputs))
                 if self.amp:
                     with amp.scale_loss(batch_outputs["loss"], self.optimizer) as scaled_loss:
@@ -84,37 +85,39 @@ class Trainer:
                 score_record["acc"].append(batch_outputs["acc"].item())
                 score_record["count"].append(len(batch_inputs["uid"]))
 
-                self.optimizer.step()
-                self.scheduler.step()
+                if (batch + 1) % self.accumulation == 0:
+                    self.optimizer.step()
+                    self.scheduler.step()
+                    self.model.zero_grad()
+                    training_results["current_iter"] += 1
 
-                training_results["current_iter"] += 1
-                if training_results["current_iter"] % self.report_interval_iters == 0:
-                    average_acc = sum(
-                        [a * c for a, c in zip(score_record["acc"], score_record["count"])]
-                    ) / sum(score_record["count"])
-                    log.info(
-                        f'train batch {batch + 1} / {len(self.task.iterators["train"])}'
-                        f'(iter {training_results["current_iter"]}),'
-                        f"current average acc {average_acc}"
-                    )
-                    score_record = {"acc": [], "count": []}
-                if training_results["current_iter"] % self.val_interval_iters == 0:
-                    val_acc = self.eval("val")["acc"]
-                    log.info(f"val acc {val_acc}")
-                    if val_acc > training_results["best_acc"]:
-                        training_results["best_acc"] = val_acc
-                        training_results["best_iter"] = training_results["current_iter"]
-                        log.info(f"best val acc updated\n{training_results}")
-                        self.save_model(os.path.join(self.exp_dir, "best_model.pt"))
-                    elif (
-                        self.stopping_patience != -1
-                        and training_results["current_iter"]
-                        > training_results["best_iter"]
-                        + self.val_interval_iters * self.stopping_patience
-                    ):
-                        log.info("out of patience")
-                        stopping = True
-                        break
+                    if training_results["current_iter"] % self.report_interval_iters == 0:
+                        average_acc = sum(
+                            [a * c for a, c in zip(score_record["acc"], score_record["count"])]
+                        ) / sum(score_record["count"])
+                        log.info(
+                            f'train batch {batch + 1} / {len(self.task.iterators["train"])}'
+                            f'(iter {training_results["current_iter"]}),'
+                            f"current average acc {average_acc}"
+                        )
+                        score_record = {"acc": [], "count": []}
+                    if training_results["current_iter"] % self.val_interval_iters == 0:
+                        val_acc = self.eval("val")["acc"]
+                        log.info(f"val acc {val_acc}")
+                        if val_acc > training_results["best_acc"]:
+                            training_results["best_acc"] = val_acc
+                            training_results["best_iter"] = training_results["current_iter"]
+                            log.info(f"best val acc updated\n{training_results}")
+                            self.save_model(os.path.join(self.exp_dir, "best_model.pt"))
+                        elif (
+                            self.stopping_patience != -1
+                            and training_results["current_iter"]
+                            > training_results["best_iter"]
+                            + self.val_interval_iters * self.stopping_patience
+                        ):
+                            log.info("out of patience")
+                            stopping = True
+                            break
                 if stopping:
                     break
 